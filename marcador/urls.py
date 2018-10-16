4d93f2f313b32a8c9f8e26291f574c5fb0f47006
dinosaurawr <dinosaurawr.1@gmail.com>
2018-10-16 22:20:47 +0300

    Added formm for bookmark adding and editing.

--- marcador/urls.py
+++ marcador/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^user/(?P<username>[-\w]+)/$', views.bookmark_user , name='marcador_bookmark_user'),
+    re_path(r'^create/$', views.bookmark_create, name='marcador_bookmark_create'),
+    re_path(r'^edit/(?P<pk>\d+)/$', views.bookmark_edit, name='marcador_bookmark_edit'),
     re_path(r'^$', views.bookmark_list, name='marcador_bookmark_list'),
 ]
